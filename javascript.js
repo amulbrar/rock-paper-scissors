ec61623c20a63eda44694f7b8302c21ebf2a1dfc
Amul <amulbrar24@gmail.com>
2024-07-13 03:15:41 +0530

    add ui

--- javascript.js
+++ javascript.js
@@ -30,17 +30,17 @@ function playRound(humanChoice, computerChoice) {
        humanChoice === "paper" && computerChoice === "rock" ||
        humanChoice === "scissors" && computerChoice === "paper") {
             humanScore = humanScore + 1;
-            resultDiv.textContent = "human beats computer";
+            resultDiv.textContent = "Human beats computer.";
     } else if(computerChoice === "rock" && humanChoice === "scissors" ||
               computerChoice === "paper" && humanChoice === "rock" ||
               computerChoice === "scissors" && humanChoice === "paper") {
                     computerScore = computerScore + 1;
-                    resultDiv.textContent = "computer beats human";
+                    resultDiv.textContent = "Computer beats human.";
     } else {
-            resultDiv.textContent = "tie!";
+            resultDiv.textContent = "Tie.";
     }
 
-    scoresDiv.textContent = `human score: ${humanScore} | computer score: ${computerScore}`;
+    scoresDiv.textContent = `Human score: ${humanScore} | Computer score: ${computerScore}`;
 
     if(humanScore === 5 || computerScore === 5){
         announceWinner();
@@ -50,11 +50,11 @@ function playRound(humanChoice, computerChoice) {
 
 function announceWinner(){
     if(humanScore > computerScore){
-        resultDiv.textContent = "human won the game";
+        resultDiv.textContent = "Human won the game.";
     } else if(computerScore > humanScore){
-        resultDiv.textContent = "computer won the game";
+        resultDiv.textContent = "Computer won the game.";
     } else {
-        resultDiv.textContent = "game over with a tie";
+        resultDiv.textContent = "Game over with a tie.";
     }
 }
 
